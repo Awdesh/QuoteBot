31a506a98acc084f686364262542a4df5b4abae3
Awdesh Sharma <arafel5@gmail.com>
2016-05-23 06:16:43 +0000

    updating script file for heroku

--- script.js
+++ script.js
@@ -1,39 +1,93 @@
 'use strict';
 
-const Script = require('smooch-bot').Script;
+const smoochBot = require('smooch-bot');
+const MemoryStore = smoochBot.MemoryStore;
+const MemoryLock = smoochBot.MemoryLock;
+const Bot = smoochBot.Bot;
+const Script = smoochBot.Script;
+const StateMachine = smoochBot.StateMachine;
 var storage = require("node-persist");
+var getQuotesFromStorage = require('../helper.js');
 storage.initSync();
 
-module.exports = new Script({
-    processing: {
-        prompt: (bot) => bot.say('Beep boop...'),
-        receive: () => 'processing'
-    },
 
+class ConsoleBot extends Bot {
+    constructor(options) {
+        super(options);
+    }
+
+    say(text) {
+        return new Promise((resolve) => {
+            console.log(text);
+            resolve();
+        });
+    }
+    
+    getQ() {
+        getQuotesFromStorage(function(quote){
+            console.log(quote);
+        });
+    }
+   
+}
+
+const script = new Script({
     start: {
         receive: (bot) => {
             return bot.say('Hi! I\'m Smooch Bot!')
-                .then(() => 'getQuotes');
+                .then(() => 'askName');
         }
     },
 
     askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+        prompt: (bot) => bot.say('What\'s your name'),
         receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
+            const name = message.text.trim();
+            bot.setProp('name', name);
+            return bot.say(`I'll call you ${name}! Great!`)
                 .then(() => 'getQuotes');
         }
     },
     
+    getQuotes : {
+      prompt: (bot) => bot.say(bot.getQ()),
+      receive: (bot) => {
+          return bot.say('Have a good day')
+            .then(() => 'finish');
+      }
+    },
+
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my crear didn't ` +
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
                         'teach me how to do anything else!'))
                 .then(() => 'finish');
         }
     }
+});
+
+const userId = 'testUserId';
+const store = new MemoryStore();
+const lock = new MemoryLock();
+const bot = new ConsoleBot({
+    store,
+    lock,
+    userId
+});
+
+const stateMachine = new StateMachine({
+    script,
+    bot,
+    userId
+});
+
+process.stdin.on('data', function(data) {
+    stateMachine.receiveMessage({
+        text: data.toString().trim()
+    })
+        .catch((err) => {
+            console.error(err);
+            console.error(err.stack);
+        });
 });
