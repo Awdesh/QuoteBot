3cae275c015ae3f3af81bd4634f94b8c98173887
Awdesh Sharma <arafel5@gmail.com>
2016-05-24 03:35:06 +0000

    script.js updated

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ module.exports = new Script({
     },
 
     getQuotes1 : {
-      prompt: (bot) => bot.say(getQuotes().toString()),
+      prompt: (bot) => bot.say(getQuotes()),
       receive: (bot) => {
           return bot.say('Have a good day')
             .then(() => 'finish');
