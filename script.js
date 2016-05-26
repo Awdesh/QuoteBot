100299902e6cd28bb137ef6baba73bffad3f0c22
Awdesh Sharma <arafel5@gmail.com>
2016-05-26 00:19:24 +0000

    updating comments

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('Good Morning..')
-                .then(() => 'getQuotes');
+                .then(() => 'askName');
         }
     },
 
@@ -44,7 +44,7 @@ Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my crear didn't ` +
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
                         'teach me how to do anything else!'))
                 .then(() => 'finish');
         }
